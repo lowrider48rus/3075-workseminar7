f8753bf467f678b68124380fe5039ec4c31fa98f
lowrider48rus <lowrider48rus@gmail.com>
2022-10-08 09:45:01 +0300

    ex047

---
+++ ex047/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+Console.WriteLine("Введи количество строк (m): ");
+int m = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введи количество столбцов (n): ");
+int n = Int32.Parse(Console.ReadLine());
+
+int[,] matrix = new int [m, n];
+
+Console.WriteLine("Введи минимальное значение: ");
+int min = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введи максимальное значение: ");
+int max = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Создан массив: ");
+FillArray(matrix, min, max);
+PrintArray(matrix);
+
+void PrintArray(int[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    Console.WriteLine();
+    }
+}
